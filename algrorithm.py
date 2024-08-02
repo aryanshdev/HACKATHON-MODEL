5f961a930a44b45d77094d258c2b2313438a3a3c
Your Name <harshitabaghel2005@gmail.com>
2024-08-02 12:28:14 +0530

    commit

--- algrorithm.py
+++ algrorithm.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score,mean_squared_error
 import joblib
